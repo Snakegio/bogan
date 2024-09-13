a8a55721edbd0f8ba6d9b1bf1b5fbaf0dd911163
MatPico <mattiapicoco06@gmail.com>
2024-09-13 17:18:55 +0200

    header add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MenubarModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+            },
+            {
+                label: 'Contact',
+            }
+        ];
+    }
 }
+
+
